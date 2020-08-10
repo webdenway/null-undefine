0a177686bc9c5f07a758d43b0a3a92736f6fb25f
webdenway <clthbbd@gmail.com>
2020-08-10 20:53:33 +0600

    null-undefine practice

---
+++ null-undefine.js
@@ -0,0 +1,29 @@
+// let memo;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+
+// }
+// const result = add(13, 82);
+
+// console.log(result);
+// function add(num1, num2) {
+//     const result = num1 + num2;
+//     return result;
+
+// }
+// const total = add(13, 82);
+
+// console.log(total);
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// console.log(add(13))
+// function add(num1, num2 = 10) {
+//     console.log(num1, num2);
+// }
+// console.log(add(13));
+
+// const people = { name: "kamal", phone: 123456, addres: "dhaka" };
+// console.log(people.id);
